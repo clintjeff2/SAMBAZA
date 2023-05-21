73941481a3e8040ec96b1a16005dba4f83001ac1
clintjeff2 <clintjeff2@gmail.com>
2023-05-21 07:28:44 +0100

    Created an update of the previous commit by making a template for the NEWSFEED body

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 
 import DefaultStructure from '../../components/dashboard/Default';
 import Card from '../../components/Card';
+import CreatePostCard from '../../components/CreatePostCard';
+import CardHeader from '../../components/CardHeader';
+
+import './../../assets/scss/components/display.scss';
+
+import './styles/main.scss';
 
 function NewsFeedIndex() {
   return (
     <DefaultStructure>
-      <Card class="content-s">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, sed? Dicta eligendi, nisi voluptatum voluptate explicabo modi qui saepe sit!
+      <Card class="content-s flex justify--between body-container" >
+        <div className="flex-width--lg body-container--main flex-col">
+          <CreatePostCard />
+          <CreatePostCard />
+          <CreatePostCard />
+        </div>
+        <div className="flex-width--md body-container--side flex-col">
+          <CardHeader title="events">
+
+          </CardHeader>
+          <CreatePostCard />
+        </div>
       </Card>
     </DefaultStructure>
 
