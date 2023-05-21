90363765bc57922c1c87081c738881cb05a0f23a
clintjeff2 <clintjeff2@gmail.com>
2023-05-21 07:32:09 +0100

    Template for the newsfeed

---
+++ src/components/CardHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './component-styles/card-header.scss';
+import './../assets/scss/components/display.scss';
+
+function CardHeader(props) {
+  return (
+    <div className='card-header flex'>
+        <div className="card-header--item title">
+            {props.title}
+        </div>
+        <div className="card-header--item action">
+            {props.children}
+        </div>
+    </div>
+  )
+}
+
+export default CardHeader;
---
+++ src/components/CreatePostCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './../assets/scss/components/display.scss';
+import './component-styles/create-post-card.scss';
+import CardHeader from './CardHeader';
+
+
+function CreatePostCard() {
+  return (
+    <div>
+        <CardHeader title="create post">
+            post
+        </CardHeader>
+        <div className="create-content">
+            <form action="" className='flex'>
+                <div className="profile-picture">
+                    <img src="" alt="" />
+                </div>
+                <input type="text" className='create-content--text' placeholder='Write post text here...'/>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default CreatePostCard
